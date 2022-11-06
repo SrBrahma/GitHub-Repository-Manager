5a5ae639a91f5ece68cde81c50e310719a631775
Henrique Bruno <henrique.bruno.fa@gmail.com>
2022-11-06 20:48:22 +0100

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,30 @@
+// https://github.com/SrBrahma/eslint-config-gev
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
 require('@rushstack/eslint-patch/modern-module-resolution');
 
+
+
 module.exports = {
-  "env": {
-    "es2021": true,
-    "node": true
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
   },
-  "extends": [
-    "plugin:@typescript-eslint/recommended-requiring-type-checking", // *1
-    "eslint-config-gev", // https://github.com/SrBrahma/eslint-config-gev-gev
+  extends: ['eslint-config-gev/js'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['eslint-config-gev/ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
+    },
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "tsconfigRootDir": __dirname, // *1
-    "project": ['./tsconfig.json'], // *1
-    "ecmaVersion": 12,
-    "sourceType": "module",
+  ignorePatterns: ['/lib/**/*', '/dist/**/*'],
+  rules: {
   },
-  "rules": {
-  }
 };
-
-// [*1] - Optional but improves the linting for Typescript:
-// https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/TYPED_LINTING.md#getting-started---linting-with-type-information
-
